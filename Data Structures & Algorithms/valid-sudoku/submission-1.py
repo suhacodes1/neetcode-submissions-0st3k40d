72d0a56210e09b1bb3ccd333d65ad1fb2821b518
suhacodes1 <suha.fathima.au@gmail.com>
2026-04-21 11:27:09 +1000

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        # Validate Rows - create a set when checking dupl
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[i][j]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+                   # return True
+        
+        # Validate Cols - 
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[j][i]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+                     #return True
+        
+        # Validate Boxes - 3 x 3 
+        start = [(0,0),(0,3),(0,6),(3,0), (3,3), (3,6), (6,0), (6,3), (6,6)]
+
+        for i, j in start:
+            s = set()
+            for row in range(i,i + 3):
+                for col in range(j, j + 3):
+                    item = board[row][col]
+                    if item in s:
+                        return False
+                    elif item != '.':
+                        s.add(item)
+        return True
+        
